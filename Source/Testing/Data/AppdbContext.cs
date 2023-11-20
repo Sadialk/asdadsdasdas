460714e71521d9239299c9fdd00d17806b519954
Clyde <klaidas.tadaravicius@gmail.com>
2023-11-20 09:02:01 +0200

    lil swap

--- Source/Testing/Data/AppdbContext.cs
+++ Source/Testing/Data/AppdbContext.cs
@@ -9,7 +9,13 @@ namespace Testing.Data
     public class AppdbContext : IdentityDbContext<RentUser>
     {
         public readonly IConfiguration _configuration;
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<City>().ToTable("Cities");
+            modelBuilder.Entity<Region>().ToTable("Regions");
+            modelBuilder.Entity<Location>().ToTable("Locations");
+        }
         public DbSet<City> cities { get; set; }
         public DbSet<Region> regions { get; set; }
         public DbSet<Location> locations { get; set; }
