8922056009fd97e48dc7c88afff10dba96182451
Clyde <klaidas.tadaravicius@gmail.com>
2023-12-14 22:01:41 +0200

    tavo mama

--- Source/Testing/Helpers/SearchParameters.cs
+++ Source/Testing/Helpers/SearchParameters.cs
@@ -2,9 +2,9 @@
 {
     public class SearchParameters
     {
-        private int _pageSize = 2;
+        private int _pageSize = 100;
         private int _pageNumber = 1;
-        private const int MaxPageSize = 50;
+        private const int MaxPageSize = 200;
         public int? PageNumber
         {
             get => _pageNumber;
